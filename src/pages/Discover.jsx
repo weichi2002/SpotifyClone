f7b0c36078d46f1f7ef5bc805ea88a77c6b80d20
weichi2002 <wchou@fandm.edu>
2022-12-12 12:56:00 -0500

    finished error and loading components

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,6 +7,11 @@ const Discover = () => {
     const genreTitle = "Pop";
     const { data, isFetching, error} = useGetTopChartsQuery();
     console.log(data);
+
+    if(isFetching) return <Loader title="Loading song..."/>;
+
+    if(error) return <Error />; 
+
     return(
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center
@@ -22,7 +27,7 @@ const Discover = () => {
 
             <div className="flex flex-wrap sm:justify-start 
             justify-center gap-8">
-                {[1,2,3,4,5,6,7,8,9,10].map((song, i) => (
+                {data?.map((song, i) => (
                 <SongCard 
                     key={song.key}
                     song={song}
