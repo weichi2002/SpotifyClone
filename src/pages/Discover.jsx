570481fb3feec985998b11291da613406711b8cd
weichi2002 <wchou@fandm.edu>
2022-12-12 03:01:43 -0500

    Configured Shazam API fetch

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,19 +1,21 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
-const genreTitle = "Pop";
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
 
 const Discover = () => {
-    console.log(genres);    
+    const genreTitle = "Pop";
+    const { data, isFetching, error} = useGetTopChartsQuery();
+    console.log(data);
     return(
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center
             sm:flex-row flex-col mt-4 mb-10">
-                <h2 class="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+                <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
                 <select 
                     onChange={()=>{}}
                     value=""
-                    className="bg-black text-gray-300 p-3 text-sm 
-                    rounded-lg outline-none sm:mt-0 mt-5">
+                    className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
                 {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
                 </select>
             </div>
@@ -27,7 +29,7 @@ const Discover = () => {
                     i={i}
                 />
                 ))}
-
+ 
             </div>
         </div>  
     );
