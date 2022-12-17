87593323c49c56dd213ee12f98590e07b15c9b95
weichi2002 <wchou@fandm.edu>
2022-12-17 16:10:34 -0500

    Finished country hot charts

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 
 const Error = () => (
-  <div className="w-full flex justify-center items center ">
-      <h1 className="font-bold text-2xl mt-2">Something went wrong... Please try again</h1>
+  <div className="w-full flex justify-center items-center h-screen">
+      <h1 className="font-bold text-2xl text-white mt-5">Something went wrong... Please try again</h1>
   </div>
 );
 
--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
+const CountryTracks = () => {
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery('US');
+
+
+  if (isFetching) return <Loader title="Loading Songs around you..." />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">USA Hot Charts</h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CountryTracks;
