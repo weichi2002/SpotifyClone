040d7e1e9d32a8204d18fb6d7082d6f4b864adbb
weichi2002 <wchou@fandm.edu>
2022-12-16 21:30:43 -0500

    finished artist page but problems in api request

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '427d3c633emsh9e776a3977474d7p1ad1f4jsn1c58c7d8d206' )
+            headers.set('X-RapidAPI-Key', '427d3c633emsh9e776a3977474d7p1ad1f4jsn1c58c7d8d206');
             return headers;
         },
     }),
@@ -13,7 +13,7 @@ export const shazamCoreApi = createApi({
         getTopCharts: builder.query({ query: () => '/charts/world'}),
         getSongsByGenre: builder.query({ query:(genre) => `/charts/genre-world?genre_code=${genre}`}),
         getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
-        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` })
